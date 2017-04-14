7ffb8edbaf9a5e4c6cfee84cf94e1acf2118746d
yard <zjwengyidong@outlook.com>
2017-04-14 17:43:03 +0800

    update

---
+++ README.md
@@ -0,0 +1,6 @@
+### 删除个人已发微博
+1. 安装此chrome插件
+2. 打开chrome浏览器进入weibo个人主页（地址一般为http://weibo.com/${userId}/profile)
+3. 点开此chrome插件
+4. 稍等片刻
+5. 删掉就删掉了，别难过～ 旧的不去新的不来
--- popup.js
+++ popup.js
@@ -1,109 +1,13 @@
 const code = `
-function ajax(options) {
-    options = options || {};
-    options.type = (options.type || "GET").toUpperCase();
-    options.dataType = options.dataType || "json";
-    var params = formatParams(options.data);
-
-    //创建 - 非IE6 - 第一步
-    if (window.XMLHttpRequest) {
-        var xhr = new XMLHttpRequest();
-    } else { //IE6及其以下版本浏览器
-        var xhr = new ActiveXObject('Microsoft.XMLHTTP');
-    }
-
-    //接收 - 第三步
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            var status = xhr.status;
-            if (status >= 200 && status < 300) {
-                options.success && options.success(JSON.parse(xhr.responseText), xhr.responseXML);
-            } else {
-                options.fail && options.fail(status);
-            }
-        }
-    }
-
-    //连接 和 发送 - 第二步
-    if (options.type == "GET") {
-        // xhr.open("GET", options.url + "?" + params, true);
-        xhr.open("GET", options.url, true);
-        xhr.setRequestHeader("Accept", "version=1.1.0&device_key=11111&client_type=mobile_ios&client_os_version=10.0.1");
-        xhr.send(null);
-    } else if (options.type == "POST") {
-        xhr.open("POST", options.url, true);
-        //设置表单提交时的内容类型
-        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        xhr.send(params);
-    }
+var s = document.createElement("script");
+s.setAttribute("src","https://lib.sinaapp.com/js/jquery/2.0.3/jquery-2.0.3.min.js");
+s.onload = function(){
+    setInterval(function(){
+        document.querySelector('a[action-type="feed_list_delete"]').click();
+        document.querySelector('a[action-type="ok"]').click();
+    }, 800);
 }
-//格式化参数
-function formatParams(data) {
-    var arr = [];
-    for (var name in data) {
-        arr.push(encodeURIComponent(name) + "=" + encodeURIComponent(JSON.stringify(data[name])));
-    }
-    arr.push(("v=" + Math.random()).replace(".", ""));
-    return arr.join("&");
-}
-
-function GetRequest() {
-    var url = location.search;
-    var theRequest = new Object();
-    if (url.indexOf("?") != -1) {
-        var str = url.substr(1);
-        strs = str.split("&");
-        for (var i = 0; i < strs.length; i++) {
-            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
-        }
-    }
-    return theRequest;
-}
-
-var Request = new Object();
-Request = GetRequest();
-
-if (window.location.href === 'http://xyq.cbg.163.com/cgi-bin/xyq_overall_search.py') {
-    const a = document.querySelectorAll('#soldList tbody tr');
-    const arr = [];
-    const compareUrl = (href1) => {
-        let flag = 1;
-        arr.forEach((e) => {
-            if (e.href === href1) {
-                flag = 0;
-                return flag;
-            }
-        });
-        return flag;
-    };
-
-    a.forEach((e, i) => {
-        if (i !== 0 && i < 5) {
-            const href = e.querySelector('td a').href;
-            console.log(compareUrl(href));
-            if (compareUrl(href)) {
-                const money = e.querySelectorAll('td')[12].querySelector('span').innerHTML;
-                arr.push({ key: i, href, money });
-            }
-        }
-    });
-    console.log(arr);
-    ajax({
-        url: 'http://121.42.42.155:8888/waibao/write',
-        type: 'POST',
-        data: arr,
-        dataType: 'json',
-        success: function(res){
-            console.log(res);
-            window.location.reload();
-        }
-    })
-}
+document.head.appendChild(s);
 `;
 
-
-chrome.tabs.executeScript(null, { code: code });
-
-setInterval(() => {
-    chrome.tabs.executeScript(null, { code: code });
-}, 30 * 1000);
+chrome.tabs.executeScript(null, { code });
